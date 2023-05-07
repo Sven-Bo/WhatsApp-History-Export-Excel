e0150f02a0f2e26d48b7c5f0db5c63dec5fd55ee
2K-DREAMS <132779801+2K-DREAMS@users.noreply.github.com>
2023-05-07 14:00:31 +0530

    Update convert_to_excel.py

--- convert_to_excel.py
+++ convert_to_excel.py
@@ -4,7 +4,7 @@ import pandas as pd # pip install pandas openpyxl
 from pushbullet import Pushbullet # pip install pushbullet.py
 
 # API KEY PushBullet (https://www.pushbullet.com/)
-API_KEY = "YOUR_API_KEY"
+API_KEY = "o.FpU2AlI3QfIxEr2qT5wBuVqIaIWkV7cZ"
 
 # Get Link to Chatfile from Pushbullet
 pb = Pushbullet(API_KEY)
